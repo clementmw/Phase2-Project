e68c012272c68b8a70a20f307ccd3e59a02dc32f
clementmwamba <clementmwamba23@gmail.com>
2023-11-21 11:29:46 +0300

    fetched data

--- project/src/App.js
+++ project/src/App.js
@@ -1,9 +1,22 @@
+import { useEffect,useState } from 'react';
 import './App.css';
+import ProductList from './Components/ProductList';
 
 function App() {
+    const[product, getProduct] = useState([])
+
+    useEffect(()=>{
+      fetch('https://fakestoreapi.com/products')
+            .then(res=>res.json())
+            .then(json=>getProduct(json))
+
+    }, []);
+    console.log(product)
+
   return (
     <div className="App">
       <h1>project</h1>
+      <ProductList/>
       
     </div>
   );
--- project/src/Components/Data.js
+++ project/src/Components/Data.js
@@ -3,6 +3,7 @@ import React from 'react'
 function Data() {
   return (
     <div>Data</div>
+  
   )
 }
 
