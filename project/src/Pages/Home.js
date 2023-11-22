a5260d3aa0bbead9e47a22822fa140da4e696bbe
Abdiqani <abdiqaniadan254@gmail.com>
2023-11-22 10:22:27 +0300

    updated card

--- project/src/Pages/Home.js
+++ project/src/Pages/Home.js
@@ -7,8 +7,14 @@ function Home() {
       <div class="card" style={{width: "18rem"}}>
   <img src="https://visionplus.co.ke/wp-content/uploads/2020/06/32.png" class="card-img-top" alt="..."/>
   <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <h5 class="card-title">32″ HD Frameless Digital TV</h5>
+    <p class="card-text">
+    HD TV 720p |
+    DVBT2/ S2 |
+    HDMI ARC |
+    Frameless
+    2yr V+ Care
+</p>
     <a href="#" class="btn btn-primary">Add to cart</a>
   </div>
 </div>
